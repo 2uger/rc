1c4c1db5cfb1fde19b844a506ca2f8b9d043332d
Oleg <oleg.ignatenko12@gmail.com>
2023-02-25 17:01:47 +0300

    clang-format

--- main/headers/peripherals.h
+++ main/headers/peripherals.h
@@ -5,8 +5,8 @@
 enum DataSrc { SWITCH, BUTTON_L, BUTTON_R, RHEOSTAT, JOYSTICK };
 
 struct Msg {
-  enum DataSrc src;
-  uint32_t payload;
+    enum DataSrc src;
+    uint32_t payload;
 };
 
 void analogue_read_t();
